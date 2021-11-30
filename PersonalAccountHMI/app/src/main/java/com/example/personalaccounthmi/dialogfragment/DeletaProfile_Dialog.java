394313d583841564a177143de5b9e30e81975624
Kaar1729 <93517635+Kaar1729@users.noreply.github.com>
2021-11-30 15:06:55 +0530

    Delete PersonalAccountHMI directory

--- PersonalAccountHMI/app/src/main/java/com/example/personalaccounthmi/dialogfragment/DeletaProfile_Dialog.java
+++
@@ -1,6 +0,0 @@
-package com.example.personalaccounthmi.dialogfragment;
-
-import com.example.personalaccounthmi.dialogfragment.PersonalAccountDialog;
-
-public class DeletaProfile_Dialog extends PersonalAccountDialog {
-}
