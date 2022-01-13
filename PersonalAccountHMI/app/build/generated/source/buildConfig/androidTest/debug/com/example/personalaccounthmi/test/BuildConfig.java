50d0f6978fefc37a9b168743d1102347732b44db
akshay.kb <akshay.kb@quest-global.com>
2022-01-13 15:41:46 +0530

    minor change

---
+++ PersonalAccountHMI/app/build/generated/source/buildConfig/androidTest/debug/com/example/personalaccounthmi/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.personalaccounthmi.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.personalaccounthmi.test";
+  public static final String BUILD_TYPE = "debug";
+}
